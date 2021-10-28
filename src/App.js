d933254be1c2df6b1387000ca2f28c6232fd5f32
gesora96 <nahidahmed9933@gmail.com>
2021-10-28 16:00:48 +0600

    password validation with regx

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import {
+  createUserWithEmailAndPassword,
   getAuth,
   GithubAuthProvider,
   GoogleAuthProvider,
@@ -6,6 +7,7 @@ import {
   signOut,
 } from 'firebase/auth';
 import { useState } from 'react';
+import { Button, Container, Form } from 'react-bootstrap';
 import './App.css';
 import initializeAuthentication from './Firebase/firebase.initialize';
 
@@ -16,6 +18,10 @@ initializeAuthentication();
 
 function App() {
   const [loggedInUser, setLoggedInUser] = useState({});
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  console.log(error);
   const auth = getAuth();
   const handleGoogleSignIn = () => {
     signInWithPopup(auth, googleProvider)
@@ -54,24 +60,94 @@ function App() {
     setLoggedInUser({});
   };
 
+  // email pass register
+
+  const handelEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handelPassword = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handelRegister = (e) => {
+    e.preventDefault();
+    if (password.length <= 8) {
+      setError('Password should be at least 8 characters');
+    } else if (!/(?=.*?[A-Z])/.test(password)) {
+      setError('Password should be at least 1 Uppercase');
+    } else if (!/(?=.*?[0-9])/.test(password)) {
+      setError('Password should be at least 1 Number');
+    } else if (!/(?=.*?[#?!@$%^&*-])/.test(password)) {
+      setError('Password should be at least 1 Spacial character');
+    } else {
+      createUserWithEmailAndPassword(auth, email, password)
+        .then((result) => {
+          const user = result.user;
+          console.log(user);
+          setError('');
+        })
+        .catch((err) => {
+          setError(err.message);
+        });
+    }
+  };
+
   return (
-    <div>
+    <Container>
+      <Form
+        className='my-5 border p-5 rounded shadow'
+        onSubmit={handelRegister}
+      >
+        <h1 className='mb-5 text-success fw-bold'>Please Registration</h1>
+        <Form.Group className='mb-3' controlId='formBasicEmail'>
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            type='email'
+            placeholder='Enter email'
+            onBlur={handelEmail}
+            required
+          />
+          <Form.Text className='text-muted'>
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className='mb-3' controlId='formBasicPassword'>
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type='password'
+            placeholder='Password'
+            onBlur={handelPassword}
+            required
+          />
+        </Form.Group>
+        <Form.Group className='mb-3' controlId='formBasicCheckbox'>
+          <Form.Check type='checkbox' label='Check me out' />
+        </Form.Group>
+        <h5 className='text-danger mb-3'>{error}</h5>
+        <Button variant='primary' type='submit'>
+          Register
+        </Button>
+      </Form>
       {!loggedInUser.name ? (
-        <span>
-          <button onClick={handleGoogleSignIn}>Google Sign In</button>
-          <button onClick={handleGitHubSignIn}>GitHub Sign In</button>
-        </span>
+        <div className='text-center py-5 rounded shadow-lg'>
+          <Button onClick={handleGoogleSignIn} className='me-5'>
+            Google Sign In
+          </Button>
+          <Button onClick={handleGitHubSignIn}>GitHub Sign In</Button>
+        </div>
       ) : (
         <button onClick={handelSignOut}>Sign Out</button>
       )}
       {loggedInUser.name && (
-        <div>
+        <div className='text-center py-5 rounded shadow-lg'>
           <h1>Welcome {loggedInUser.name} </h1>
           <p>Your Email: {loggedInUser.email}</p>
           <img src={loggedInUser.img} alt='' />
         </div>
       )}
-    </div>
+    </Container>
   );
 }
 

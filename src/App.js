626af6350503a975a8344bce9338e20ff0432067
gesora96 <nahidahmed9933@gmail.com>
2021-10-28 17:39:30 +0600

    reset password & varification email added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import {
   getAuth,
   GithubAuthProvider,
   GoogleAuthProvider,
+  sendEmailVerification,
+  sendPasswordResetEmail,
   signInWithEmailAndPassword,
   signInWithPopup,
   signOut,
@@ -22,6 +24,7 @@ function App() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
+  const [success, setSuccess] = useState('');
   const [isRegister, setIsRegister] = useState(false);
   const auth = getAuth();
 
@@ -36,6 +39,7 @@ function App() {
           img: photoURL,
           email,
         });
+        setError('');
       })
       .catch((err) => {
         setError(err.message);
@@ -53,6 +57,7 @@ function App() {
           img: photoURL,
           email,
         });
+        setError('');
       })
       .catch((err) => {
         setError(err.message);
@@ -94,10 +99,13 @@ function App() {
         .then((result) => {
           const user = result.user;
           console.log(user);
+          sendEmailVerification(auth.currentUser);
           setError('');
+          setSuccess('we send an verification mail. check now!!!');
         })
         .catch((err) => {
           setError(err.message);
+          setSuccess('');
         });
     }
   };
@@ -105,13 +113,33 @@ function App() {
   // login
 
   const loginUser = (email, password) => {
-    signInWithEmailAndPassword(auth, email, password)
-      .then((result) => {
-        const user = result.user;
-        console.log(user);
+    if (auth.currentUser.emailVerified) {
+      signInWithEmailAndPassword(auth, email, password)
+        .then((result) => {
+          const user = result.user;
+          console.log(user);
+          setError('');
+          setSuccess('Log In successfully');
+        })
+        .catch((err) => {
+          setError(err.message);
+          setSuccess('');
+        });
+    } else {
+      console.log('asd');
+      setSuccess('');
+      setError('Place check your mail. we send a verification mail');
+    }
+  };
+
+  const resetPassword = () => {
+    sendPasswordResetEmail(auth, email)
+      .then(() => {
+        setSuccess('Send a mail on your gmail account');
       })
       .catch((err) => {
         setError(err.message);
+        setSuccess('');
       });
   };
 
@@ -160,9 +188,17 @@ function App() {
             onChange={handelRegistrationChange}
           />
         </Form.Group>
-        <h5 className='text-danger mb-3'>{error}</h5>
-        <Button variant='primary' type='submit'>
+        {success ? (
+          <h5 className='text-success mb-3'>{success}</h5>
+        ) : (
+          <h5 className='text-danger mb-3'>{error}</h5>
+        )}
+
+        <Button variant='primary' type='submit' className='me-5'>
           {isRegister ? 'Login ' : 'Registration'}
+        </Button>
+        <Button onClick={resetPassword} variant='warning'>
+          Reset PassWord
         </Button>
       </Form>
       {!loggedInUser.name ? (

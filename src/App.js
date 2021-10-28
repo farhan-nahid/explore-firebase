6927ffd083bde6c8171408b98cb47f26a9a5b66e
gesora96 <nahidahmed9933@gmail.com>
2021-10-28 18:45:41 +0600

    email auth added

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import {
   signInWithEmailAndPassword,
   signInWithPopup,
   signOut,
+  updateProfile,
 } from 'firebase/auth';
 import { useState } from 'react';
 import { Button, Container, Form } from 'react-bootstrap';
@@ -16,11 +17,11 @@ import initializeAuthentication from './Firebase/firebase.initialize';
 
 const googleProvider = new GoogleAuthProvider();
 const gitHubProvider = new GithubAuthProvider();
-// googleProvider.addScope('https://www.googleapis.com/auth/contacts.readonly');
 initializeAuthentication();
 
 function App() {
   const [loggedInUser, setLoggedInUser] = useState({});
+  const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
@@ -66,6 +67,10 @@ function App() {
 
   // email pass auth
 
+  const handelName = (e) => {
+    setName(e.target.value);
+  };
+
   const handelEmail = (e) => {
     setEmail(e.target.value);
   };
@@ -96,10 +101,15 @@ function App() {
       setError('Password should be at least 1 Spacial character');
     } else {
       createUserWithEmailAndPassword(auth, email, password)
-        .then((result) => {
-          const user = result.user;
-          console.log(user);
+        .then(() => {
+          // const user = result.user;
+          // console.log(user);
+          updateProfile(auth.currentUser, {
+            photoURL: 'https://i.ibb.co/G31TsrC/user.png',
+            displayName: name,
+          });
           sendEmailVerification(auth.currentUser);
+
           setError('');
           setSuccess('we send an verification mail. check now!!!');
         })
@@ -116,8 +126,12 @@ function App() {
     if (auth.currentUser.emailVerified) {
       signInWithEmailAndPassword(auth, email, password)
         .then((result) => {
-          const user = result.user;
-          console.log(user);
+          const { displayName, photoURL, email } = result.user;
+          setLoggedInUser({
+            name: displayName,
+            img: photoURL,
+            email,
+          });
           setError('');
           setSuccess('Log In successfully');
         })
@@ -126,7 +140,6 @@ function App() {
           setSuccess('');
         });
     } else {
-      console.log('asd');
       setSuccess('');
       setError('Place check your mail. we send a verification mail');
     }
@@ -152,70 +165,92 @@ function App() {
 
   return (
     <Container>
-      <Form
-        className='my-5 border p-5 rounded shadow'
-        onSubmit={handelRegister}
-      >
-        <h1 className='mb-5 text-success fw-bold'>
-          Please {isRegister ? 'Login ' : 'Registration'}
-        </h1>
-        <Form.Group className='mb-3' controlId='formBasicEmail'>
-          <Form.Label>Email address</Form.Label>
-          <Form.Control
-            type='email'
-            placeholder='Enter email'
-            onBlur={handelEmail}
-            required
-          />
-          <Form.Text className='text-muted'>
-            We'll never share your email with anyone else.
-          </Form.Text>
-        </Form.Group>
-
-        <Form.Group className='mb-3' controlId='formBasicPassword'>
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type='password'
-            placeholder='Password'
-            onBlur={handelPassword}
-            required
-          />
-        </Form.Group>
-        <Form.Group className='mb-3' controlId='formBasicCheckbox'>
-          <Form.Check
-            type='checkbox'
-            label='Already Registered ?'
-            onChange={handelRegistrationChange}
-          />
-        </Form.Group>
-        {success ? (
-          <h5 className='text-success mb-3'>{success}</h5>
-        ) : (
-          <h5 className='text-danger mb-3'>{error}</h5>
-        )}
-
-        <Button variant='primary' type='submit' className='me-5'>
-          {isRegister ? 'Login ' : 'Registration'}
-        </Button>
-        <Button onClick={resetPassword} variant='warning'>
-          Reset PassWord
-        </Button>
-      </Form>
       {!loggedInUser.name ? (
-        <div className='text-center py-5 rounded shadow-lg'>
-          <Button onClick={handleGoogleSignIn} className='me-5'>
-            Google Sign In
-          </Button>
-          <Button onClick={handleGitHubSignIn}>GitHub Sign In</Button>
-        </div>
+        <>
+          <Form
+            className='my-5 border p-5 rounded shadow'
+            onSubmit={handelRegister}
+          >
+            <h1 className='mb-5 text-success fw-bold'>
+              Please {isRegister ? 'Login' : 'Registration'} in our Website
+            </h1>
+            {!isRegister && (
+              <Form.Group className='mb-3' controlId='formBasicName'>
+                <Form.Label>Your Name</Form.Label>
+                <Form.Control
+                  type='name'
+                  placeholder='Enter Name'
+                  onBlur={handelName}
+                  required
+                />
+              </Form.Group>
+            )}
+
+            <Form.Group className='mb-3' controlId='formBasicEmail'>
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                type='email'
+                placeholder='Enter email'
+                onBlur={handelEmail}
+                required
+              />
+              <Form.Text className='text-muted'>
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+
+            <Form.Group className='mb-3' controlId='formBasicPassword'>
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type='password'
+                placeholder='Password'
+                onBlur={handelPassword}
+                required
+              />
+            </Form.Group>
+            <Form.Group className='mb-3' controlId='formBasicCheckbox'>
+              <Form.Check
+                type='checkbox'
+                label='Already Registered ?'
+                onChange={handelRegistrationChange}
+              />
+            </Form.Group>
+            {success ? (
+              <h5 className='text-success mb-3'>{success}</h5>
+            ) : (
+              <h5 className='text-danger mb-3'>{error}</h5>
+            )}
+
+            <Button variant='primary' type='submit' className='me-5'>
+              {isRegister ? 'Login' : 'Registration'}
+            </Button>
+            {isRegister && (
+              <Button onClick={resetPassword} variant='warning'>
+                Reset PassWord
+              </Button>
+            )}
+          </Form>
+
+          <div className='text-center py-5 rounded shadow-lg'>
+            <Button onClick={handleGoogleSignIn} className='me-5'>
+              Google Sign In
+            </Button>
+            <Button onClick={handleGitHubSignIn}>GitHub Sign In</Button>
+          </div>
+        </>
       ) : (
-        <button onClick={handelSignOut}>Sign Out</button>
-      )}
-      {loggedInUser.name && (
         <div className='text-center py-5 rounded shadow-lg'>
           <h1>Welcome {loggedInUser.name} </h1>
           <p>Your Email: {loggedInUser.email}</p>
-          <img src={loggedInUser.img} alt='' />
+          <img
+            className='rounded-circle'
+            src={loggedInUser.img}
+            alt={loggedInUser.name}
+          />{' '}
+          <br /> <br />
+          <Button variant='danger' onClick={handelSignOut}>
+            Sign Out
+          </Button>
         </div>
       )}
     </Container>
